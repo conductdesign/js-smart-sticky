903631bffd413c144746afc986be5860996ae2c9
conductdesign <git@conductdesign.com>
2016-07-10 14:49:45 +0200

    debug; v0.3

--- js/html5-fixed-sidebar.js
+++ js/html5-fixed-sidebar.js
@@ -1,6 +1,6 @@
 /**
- * A Vanilla JS HTML5 Fixed Sidebar 
- * Version: 0.2
+ * JS HTML5 Fixed Sidebar 
+ * Version: 0.3
  * 
  * Author: Chris Freeman
  * Author URL: http://www.conductdesign.com
@@ -13,11 +13,11 @@
  * License URL: http://www.gnu.org/licenses/gpl-3.0.html
  *
  * Options: 
- * sidebarID  : The ID of the element to be positioned. Must be, obviously, a unique ID. (Default: false – uses first <aside> in DOM) 
- * parentID   : If, for whatever reason, the parent is not the immediate container. (Default: false – uses parentElement of the sidebar) 
- * minWidth		: Set breakpoint (px) for responsive mobile layout. Plugin functionality is disabled for window widths below this value. (Default: false)
- * adminbarID : The ID of an admin- or controlbar element, e.g. 'wpadminbar' for the WordPress crowd. (Default: false)
-
+ * sidebarID  : String. The ID of the element to be positioned. Must be, obviously, a unique ID. (Default: false – uses first <aside> in DOM) 
+ * parentID   : String. If, for whatever reason, the parent is not the sidebar's immediate container. (Default: false – uses parentElement of the sidebar) 
+ * minWidth		: Number. Set breakpoint (px) for responsive mobile layout. Plugin functionality is disabled for window widths below this value. (Default: false)
+ * topOffsetIDs : Array. The ID or IDs of any FIXED (position:fixed) elements, such as a masthead or admin- or controlbar element, e.g. array('wpadminbar', 'masthead'). (Default: empty array())
+ * bottomOffsetIDs : Array. The ID or IDs of any FIXED (position:fixed) footer elements, such as a site footer or cookies warning, e.g. array('colophon'). (Default: empty array())
  */
  
 
@@ -30,9 +30,9 @@
       	sidebarID 	: false,
 				parentID		: false,
       	minWidth		: false,
-			//topOffset		: 0,
-			//bottomOffset: 0,
-				adminbarID	: false
+				topOffsetIDs	 : [],
+				bottomOffsetIDs: [],
+				//adminbarID	: false
     };
 		
 		// Create options by extending defaults with the passed in arugments
@@ -53,15 +53,6 @@
 		//use direct parent element if not user-defined. 
 		this.parent = document.getElementById(this.options.parentID) || this.el.parentElement;
 		
-		// check for adminbar element
-		if (this.options.adminbarID) { 
-			this.adminbar = document.getElementById(this.options.adminbarID);
-
-			if ( ! this.adminbar ) { 
-				//console.log('Cannot find element with ID "' + this.options.adminbarID + '". Remember to remove leading hash symbol.');
-			}
-		}
-		
 		// set global flags for the scroll handler
 		this.lastWindowPos = pageYOffset || (this.docElem.clientHeight ? this.docElem.scrollTop : this.body.scrollTop); // accounts for page reloads
 		this.top = false;
@@ -79,7 +70,7 @@
 	};
 	
 	// Public Methods
-	
+	// none.
 	
 	// Private Methods
 	
@@ -109,14 +100,22 @@
 		this.windowWidth  = this.window.innerWidth || this.docElem.offsetWidth || this.body.offsetWidth;            
 		
 		// check the admin bar height with each resize.
-		this.adminbarOffset = (this.adminbar) ? getBounds.call(this, this.adminbar).height : 0;
+		this.topOffset = 0;
+		// check topOffsetIDs option
+		if (this.options.topOffsetIDs.length > 0) { 
+			for (var i = 0; i < this.options.topOffsetIDs.length; i++) {
+				this.topOffset += getBounds.call(this, document.getElementById(this.options.topOffsetIDs[i])).height;  
+			}
+		}
+		console.log(this.topOffset);
 
 		// reset element CSS and attachment flags
 		this.el.style.cssText = '';
 		this.top = this.bottom = this.fixed = false;
 		
 		// Get element width AFTER resetting css and BEFORE recalulating sidebar position to ensure width relative to parent.
-		this.elWidth = getBounds.call(this, this.el).width;
+		this.elWidth 		 = getBounds.call(this, this.el).width;
+		this.elTopOffset = getBounds.call(this, this.el).top;
 		
 		setScrollHandler.call(this);
 		
@@ -141,30 +140,31 @@
 				
 				if ( this.top ) {
 					this.top = false;
-					topOffset = ( elBounds.top > 0 ) ? elBounds.top - trackBounds.top - this.adminbarOffset : 0;
-					this.el.style.cssText = 'top:' + topOffset + 'px';
+					console.log(elBounds.top);
+					topOffset = ( elBounds.top > this.topOffset ) ? elBounds.top - trackBounds.top : 0;
+					this.el.style.cssText = 'top:' + topOffset + 'px; content:"down1";';
 				
 				} else if ( ! this.bottom && windowBottom > elBounds.bottom && elBounds.bottom < trackBounds.bottom ) { 
 					this.bottom = true;
 					this.el.style.cssText = 'position:fixed; bottom:0px; width:' + this.elWidth + 'px';
 				
-				} else if ( this.bottom && windowBottom >= trackBounds.bottom + this.adminbarOffset ) {
+				} else if ( this.bottom && windowBottom >= trackBounds.bottom ) {
 					this.bottom = false;
-					this.el.style.cssText = 'top:' + (trackBounds.bottom - elBounds.height - trackBounds.top) + 'px';
+					this.el.style.cssText = 'top:' + (trackBounds.height - elBounds.height) + 'px; content:"flag:3"';
 				}
 			   
 			} else if ( windowPos < this.lastWindowPos ) { // scrolling up
 			
 				if ( this.bottom ) {
 					this.bottom = false;
-					topOffset = ( elBounds.top > 0 ) ? elBounds.top - trackBounds.top - this.adminbarOffset : 0;
-					this.el.style.cssText = 'top:' + topOffset + 'px';
+					topOffset = ( elBounds.top > trackBounds.top ) ? elBounds.top - trackBounds.top : 0;
+					this.el.style.cssText = 'top:' + topOffset + 'px; content:"up1";';
 				
-				} else if ( ! this.top && windowPos + this.adminbarOffset < elBounds.top && windowPos > trackBounds.top ) {
+				} else if ( ! this.top && windowPos + this.topOffset < elBounds.top && windowPos + this.topOffset > trackBounds.top ) {
 					this.top = true;
-					this.el.style.cssText = 'position:fixed; top:' + this.adminbarOffset + 'px; width:' + this.elWidth + 'px';
+					this.el.style.cssText = 'position:fixed; top:' + this.topOffset + 'px; width:' + this.elWidth + 'px';
 				
-				} else if ( this.top && windowPos <= trackBounds.top ) {
+				} else if ( this.top && windowPos + this.topOffset <= trackBounds.top ) {
 					this.el.style.cssText = '';
 				}
 				
@@ -173,7 +173,7 @@
 				
 				if ( windowPos + elBounds.height < trackBounds.bottom && windowPos > trackBounds.top ) {
 					this.top = true;
-					this.el.style.cssText = 'position:fixed; top:' + this.adminbarOffset + 'px; width:' + this.elWidth + 'px';
+					this.el.style.cssText = 'position:fixed; top:' + this.topOffset + 'px; width:' + this.elWidth + 'px';
 				
 				} else if ( windowBottom >= trackBounds.bottom ) {
 					this.bottom = true;
@@ -190,33 +190,33 @@
 			
 			if (windowPos > this.lastWindowPos) { // scrolling down
 				
-				if (!this.fixed && windowPos + this.adminbarOffset > elBounds.top && elBounds.bottom < trackBounds.bottom) {
+				if (!this.fixed && windowPos + this.topOffset > elBounds.top && elBounds.bottom < trackBounds.bottom) {
 					this.fixed = true;
-					this.el.style.cssText = 'position:fixed; top:' + this.adminbarOffset + 'px; width:' + this.elWidth + 'px';
+					this.el.style.cssText = 'position:fixed; top:' + this.topOffset + 'px; width:' + this.elWidth + 'px';
 
-				} else if (this.fixed && elBounds.bottom - this.adminbarOffset >= trackBounds.bottom) {
+				} else if (this.fixed && elBounds.bottom - this.topOffset >= trackBounds.bottom) {
 					this.fixed = false;
 					this.el.style.cssText = 'top:' + (trackBounds.bottom - elBounds.height - trackBounds.top) + 'px';
 				} 	
 				
 			} else if (windowPos < this.lastWindowPos) { // scrolling up
 				
-				if (this.fixed && windowPos <= trackBounds.top) {
+				if (this.fixed && (windowPos + this.topOffset) <= trackBounds.top) {
 					this.fixed = false;
 					this.el.style.cssText = '';
 
-				} else if (!this.fixed && windowPos < elBounds.top - this.adminbarOffset && windowPos > trackBounds.top) {
+				} else if (!this.fixed && windowPos < elBounds.top - this.topOffset && windowPos > trackBounds.top) {
 					this.fixed = true;
-					this.el.style.cssText = 'position:fixed; top:' + this.adminbarOffset + 'px; width:' + this.elWidth + 'px';
+					this.el.style.cssText = 'position:fixed; top:' + this.topOffset + 'px; width:' + this.elWidth + 'px';
 				}
 				
 			} else { // no scroll, but probably a resize
 				this.fixed = false;
-				if (windowPos + elBounds.height + this.adminbarOffset < trackBounds.bottom && windowPos > trackBounds.top) {
+				if (windowPos + elBounds.height + this.topOffset < trackBounds.bottom && windowPos > trackBounds.top) {
 					this.fixed = true;
-					this.el.style.cssText = 'position:fixed; top:0; width:' + this.elWidth + 'px';
+					this.el.style.cssText = 'position:fixed; top:' + this.topOffset + 'px; width:' + this.elWidth + 'px';
 				
-				} else if (windowBottom >=trackBounds.bottom) {
+				} else if (windowBottom >= trackBounds.bottom) {
 					this.el.style.cssText = 'top:' + (trackBounds.bottom - elBounds.height - trackBounds.top) + 'px';
 				
 				} else {
@@ -230,6 +230,7 @@
 	
 	
 	function getBounds (elem) {
+		if (elem === null) {return { top: 0, bottom: 0, width: 0, height: 0 };}
 		var rect, 
 				top, 
 				bottom, 
@@ -261,6 +262,5 @@
 		}
 		return source;
 	}
-	
 	
 }());	
