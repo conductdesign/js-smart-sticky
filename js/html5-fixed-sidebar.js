36383142217c90ac370b18ca36c7508c8c5964a8
conductdesign <git@conductdesign.com>
2016-09-06 23:14:22 +0200

    major overhaul to 'track' size calculation

--- js/html5-fixed-sidebar.js
+++ js/html5-fixed-sidebar.js
@@ -1,6 +1,6 @@
 /**
  * JS HTML5 Fixed Sidebar 
- * Version: 0.4
+ * Version: 0.5
  * 
  * Author: Chris Freeman
  * Author URL: http://www.conductdesign.com
@@ -19,7 +19,6 @@
  * @param		bottomOffsetIDs	Array.	The ID or IDs of any FIXED (position:fixed) footer elements, e.g. array('colophon'). (Default: empty array)
  */
  
-
 (function() {
 	
 	this.Sticker = function () {
@@ -45,12 +44,16 @@
 		this.docElem = document.documentElement;
 		this.clientTop = this.docElem.clientTop || this.body.clientTop || 0;
 		
+		// check for requisite functionality before proceeding
+		if ( ! this.body.getBoundingClientRect || ! getComputedStyle ) { return false;}
+		
 		// Dynamic properties
 		this.el = document.getElementById(this.options.sidebarID) || document.getElementsByTagName('aside')[0];
-		if (this.el === null) {return;}
+		if (this.el === null) {return false;}
 		
 		//use direct parent element if not user-defined. 
 		this.parent = document.getElementById(this.options.parentID) || this.el.parentElement;
+		console.log(this.parent);
 		
 		// set global flags for the scroll handler
 		this.lastWindowPos = pageYOffset || (this.docElem.clientHeight ? this.docElem.scrollTop : this.body.scrollTop); // accounts for page reloads
@@ -113,7 +116,7 @@
 		this.windowWidth  = this.window.innerWidth || this.docElem.offsetWidth || this.body.offsetWidth;   
 		
 		
-		// check the admin bar height with each resize.
+		// get combined height of any fixed headers and admin bars.
 		this.topOffset = 0;
 		// check topOffsetIDs option
 		if (this.options.topOffsetIDs.length > 0) { 
@@ -121,13 +124,23 @@
 				this.topOffset += getBounds.call(this, document.getElementById(this.options.topOffsetIDs[i])).height;  
 			}
 		}
-		// console.log(this.topOffset);
+		console.log(this.topOffset);
+		
+		
+		this.bottomOffset = 0;
+		// get combined height of any absolute positioned footer elements.
+		if (this.options.bottomOffsetIDs.length > 0) { 
+			for (var i = 0; i < this.options.bottomOffsetIDs.length; i++) {
+				this.bottomOffset += getBounds.call(this, document.getElementById(this.options.bottomOffsetIDs[i])).height;  
+			}
+		}
+		console.log(this.bottomOffset);
 
 		// reset element CSS and attachment flags
 		this.el.style.cssText = '';
 		this.top = this.bottom = this.fixed = false;
 		
-		// Get element width AFTER resetting css and BEFORE recalulating sidebar position to ensure width relative to parent.
+		// Get element width AFTER resetting css and BEFORE recalculating sidebar position to ensure width relative to parent.
 		this.elWidth 		 = getBounds.call(this, this.el).width;
 		this.elTopOffset = getBounds.call(this, this.el).top;
 		
@@ -156,11 +169,11 @@
 					topOffset = ( elBounds.top > this.topOffset ) ? elBounds.top - trackBounds.top : 0;
 					this.el.style.cssText = 'top:' + topOffset + 'px; content:"down1";';
 				
-				} else if ( ! this.bottom && windowBottom > elBounds.bottom && elBounds.bottom < trackBounds.bottom ) { 
+				} else if ( ! this.bottom && windowBottom > elBounds.bottom && elBounds.bottom < trackBounds.bottom - this.bottomOffset ) { 
 					this.bottom = true;
 					this.el.style.cssText = 'position:fixed; bottom:0px; width:' + this.elWidth + 'px';
 				
-				} else if ( this.bottom && windowBottom >= trackBounds.bottom ) {
+				} else if ( this.bottom && windowBottom >= trackBounds.bottom - this.bottomOffset ) {
 					this.bottom = false;
 					this.el.style.cssText = 'top:' + (trackBounds.height - elBounds.height) + 'px; content:"flag:3"';
 				}
@@ -183,13 +196,13 @@
 			} else { // no scroll, but probably a resize
 				this.top = this.bottom = false;
 				
-				if ( windowPos + elBounds.height < trackBounds.bottom && windowPos > trackBounds.top ) {
+				if ( windowPos + elBounds.height < (trackBounds.bottom - this.bottomOffset) && windowPos > trackBounds.top ) {
 					this.top = true;
 					this.el.style.cssText = 'position:fixed; top:' + this.topOffset + 'px; width:' + this.elWidth + 'px';
 				
-				} else if ( windowBottom >= trackBounds.bottom ) {
+				} else if ( windowBottom >= (trackBounds.bottom - this.bottomOffset) ) {
 					this.bottom = true;
-					this.el.style.cssText = 'top:' + (trackBounds.bottom - elBounds.height - trackBounds.top) + 'px';
+					this.el.style.cssText = 'top:' + (trackBounds.bottom - this.bottomOffset - elBounds.height - trackBounds.top) + 'px';
 				
 				} else {
 					this.top = true;
@@ -242,28 +255,47 @@
 	
 	
 	function getBounds (elem) {
-		if (elem === null) {return { top: 0, bottom: 0, width: 0, height: 0 };}
+		if (elem === null) {return false;}
 		var rect, 
 				top, 
 				bottom, 
 				width,
 				height,
+				innerTop,
+				innerBottom,
 				scrollTop = window.pageYOffset || this.docElem.scrollTop || this.body.scrollTop,
-				scrollLeft = window.pageXOffset || this.docElem.scrollLeft || this.body.scrollLeft;
-				
+				scrollLeft = window.pageXOffset || this.docElem.scrollLeft || this.body.scrollLeft,
+				cs = getComputedStyle(elem) || false;
+		
 		if (elem.getBoundingClientRect) { // Internet Explorer, Firefox 3+, Google Chrome, Opera 9.5+, Safari 4+
 			rect		= elem.getBoundingClientRect();
 			top			= rect.top + scrollTop - this.clientTop;
 			bottom	= rect.bottom + scrollTop - this.clientTop;
 			width		= rect.right - rect.left;
 			height	= rect.bottom - rect.top;
+			if (cs) {
+				innerTop		= rect.top + parseFloat(cs.paddingTop) + parseFloat(cs.borderTopWidth);
+				innerBottom = rect.bottom - parseFloat(cs.paddingBottom) - parseFloat(cs.borderBottomWidth);
+			} else {
+				innerTop		= rect.top;
+				innerBottom = rect.bottom;
+				console.log("Error: cannot use getComputedStyle!");
+			}
+			
 		} else {
-			//console.log("Error: cannot use getBoundingClientRect()");
+			console.log("Error: cannot use getBoundingClientRect!");
 		}
 		
-		return { top: Math.round(top), bottom: Math.round(bottom), width: Math.round(width), height: Math.round(height) };	
+		return { 
+			top: Math.round(top), 
+			bottom: Math.round(bottom), 
+			width: Math.round(width), 
+			height: Math.round(height), 
+			innerTop: Math.round(innerTop),
+			innerBottom: Math.round(innerBottom),
+		};	
 	}
-	
+
 	
 	function extendDefaults(source, properties) {
 		var property;
